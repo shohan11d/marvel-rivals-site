55823250690ecd740dfb03fa65f7409017d3ee2a
shohan11d <shohan11d@gmail.com>
2026-01-01 14:13:05 +0600

    conttact added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <About/>
       <Features />
       <Story />
+      <Contact />
       <Footer />
     </>
   );
