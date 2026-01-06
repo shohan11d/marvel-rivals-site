7284c92a66bb65f20443bf392cb2fb172fc27002
shohan11d <shohan11d@gmail.com>
2026-01-06 17:21:21 +0600

    about section added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,42 +2,62 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 
-/* function About() {
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h1 className="hero-heading text-black">
-          Discover the worlds largest shared Adventure
-        </h1>
-        <img
-          src="img/about.webp"
-          alt="Background"
-          className="absolute left-0 top-0 w-full h-[530px] object-cover"
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </p>
+
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 text-black! text-center"
         />
+
         <div className="about-subtext">
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
           <p className="text-gray-500">
             Zentry unites every player from countless games and platforms, both
             digital and physical, into a unified Play Economy
           </p>
         </div>
       </div>
-    </div>
-  );
-} */
 
-function About() {
-  return (
-    <div id="about" className="min-h-screen w-screen py-20 ">
-      <h1 className="font-zentry text-7xl md:text-8xl  mx-6  lg:mx-60 text-center text-black mb-12">
-        Discover the worlds largest shared Adventure
-      </h1>
-      <img
-        src="img/about.webp"
-        alt="Background"
-        className="h-[530px] md:w-[530px] rounded-2xl object-cover mx-auto"
-      />
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default About;
